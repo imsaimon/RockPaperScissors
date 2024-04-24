7f5a8ec3ed7a572dad8295714e2eb7bcfe25a0a0
imsaimon <imthesaimon@gmail.con>
2024-04-24 11:14:18 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+# Write your code below this line 👇
+print("Welcome to the Rock, Paper, Scissors Game!")
+choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+if choice == 0:
+    print(paper)
+if choice == 1:
+    print(scissors)
+if choice == 2:
+    print(rock)
+
+print("Computer chose:")
+import random
+
+random_choice = random.randint(0, 2)
+if random_choice == 0:
+    print(paper)
+if random_choice == 1:
+    print(scissors)
+if random_choice == 2:
+    print(rock)
